10a56a8ca6ef5a50553674a47ac05a80a3de940a
Aaron Jarecki <aaronjarecki@gmail.com>
2014-03-14 15:32:36 -0400

    Aaron: parsing file

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
 )
 
+type RankNode struct {
+	Name    string
+	Rank    float32
+	Related []RankNode
+}
+
 func main() {
-	fmt.Printf("Eventually this will read a gv file and figure out the "PageRank" of each node\n")
+	body, err := ioutil.ReadFile("test.gv")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	relationRegEx := regexp.MustCompile("([A-z][A-z]*) *-> *([A-z][A-z]*)")
+	for _, line := range strings.Split(string(body), "\n") {
+
+	}
+	fmt.Printf("%v\n", string(body))
 }
